d6f2e1cd8f34d518ea78fcea4c823246fe9357f6
Gabriel Abreu <gabrielabreualves@gmail.com>
2022-10-06 00:03:20 -0300

    resolução dos exercícios em js bee 1182,1183,1184

---
+++ javascript/bee1182.js
@@ -0,0 +1,26 @@
+const input = require("fs").readFileSync("stdin", "utf-8");
+const lines = input.split("\n");
+
+const coluna = Number(lines.shift())
+const operacao = lines.shift().trim()
+let soma = 0
+let m = []
+
+// Preenchendo a matriz
+for(let i = 0; i < 12; i++) {
+    m[i] = []
+    for(let j = 0; j < 12; j++) {
+        m[i][j] = Number(lines.shift())
+        if(j == coluna) {
+            // Soma os elementos da matriz na coluna especificada
+            soma += m[i][j]
+        }
+    }
+}
+
+if(operacao == 'S') {
+    console.log(soma.toFixed(1))
+} else {
+    let media = soma / 12
+    console.log(media.toFixed(1))
+}
